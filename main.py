a97be80d961116415e76eb153f3726af2ece08c3
jarivankaam <D281531@edu.curio.nl>
2024-04-11 21:14:06 +0200

    [Todo-app-PY] - Add getLists function

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 #! usr/bin/env python3
 
+import os
+
+def getLists():
+
+    lists = []
+    dirs = os.listdir('./lists')
+
+    if dirs != []:
+        for dir in dirs:
+            with open(f'./lists/{dir}', 'r') as l:
+                for task in l:
+                    lists.append(task)
+
+    return lists
+            
+        
+
+
 def main():
-    print('Hello World')
+    lists = getLists()
+    print(lists)
 
 
 if __name__ == '__main__':
